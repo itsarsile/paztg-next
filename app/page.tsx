ad7f57d489e9f192983f50e7d68f1fa89373682b
Arsile <ajiprio@hotmail.com>
2023-05-21 01:55:02 +0700

    First page done

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className={`flex flex-col gap-6 items-center justify-center w-screen h-screen from-black via-slate-900/20 to-black`}>
-      <div className={`text-center space-y-2`}>
-        <h1 className={`text-5xl md:text-9xl font-montserrat font-black uppercase`}>paztg</h1>
-        <h2 className={`tracking-[0.25rem] text-xs md:text-4xl font-montserrat font-light`}>Motionworks</h2>
-      </div>
+    // <main className={`flex flex-col gap-6 items-center justify-center w-screen h-screen from-black via-slate-900/20 transition-colors to-red-600/20 bg-gradient-radial`}>
+      <main className={`flex flex-col gap-6 items-center justify-center w-screen h-screen bg-gradient-to-tl from-amber-900/20 via-slate-900/20 to-amber-900/50 animate-gradient-fade-in`}>
       <div>
+        <h2 className={`text-xs md:text-xl tracking-widest font-montserrat uppercase font-light animate-fade-in`}>Motionworks</h2>
+      </div>
+      <div className={`text-center space-y-2`}>
+        <h1 className={`text-5xl md:text-9xl duration-1000 font-montserrat font-black uppercase animate-title`}>paztg</h1>
+      </div>
         <nav>
-          <ul className='flex list-none gap-3 text-xs text-zinc-500'>
+          <ul className='flex list-none gap-3 text-xl text-zinc-500 justify-center animate-fade-in'>
             <li>Projects</li>
             <li>About</li>
           </ul>
         </nav>
-      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,45 @@ module.exports = {
       },
       fontFamily: {
         montserrat: ['var(--font-montserrat)']
+      },
+      animation: {
+        title: 'title 3s ease-out forwards',
+        "fade-in": "fade-in 3s ease-in-out forwards",
+        "gradient-fade-in": "gradient-fade-in 3s forwards"
+      },
+      keyframes: {
+        'gradient-fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        },
+        "title": {
+          '0%': {
+            opacity: '0%',
+            'letter-spacing': '0.5em'
+          },
+          '25%': {
+            'line-height': '0%',
+            opacity: '0%'
+          },
+          '75%': {
+            opacity: '10%',
+          },
+          '100%': {
+            'line-height': '100%',
+            opacity: '100%'
+          }
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0%"
+          },
+          "75%": {
+            opacity: "0%"
+          },
+          "100%": {
+            opacity: "100%"
+          }
+        }
       }
     },
   },
